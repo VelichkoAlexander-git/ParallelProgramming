af463d134bbb261b860cfac181b4382f787619a6
Alexander Velichko <Alexander-Velichko@bk.ru>
2020-11-28 18:10:05 +0200

    task

---
+++ LaboratoryWorkFive/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaboratoryWorkFive
+{
+    /*
+     * Реализуйте Windows-приложение, которое последовательно отображает состояния клеточной модели «Игра жизнь».
+     */
+
+    public partial class Form1 : Form
+    {
+        GameLife Game;
+        private Graphics graphics;
+        private int resolution;
+        private int rows;
+        private int cols;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.Black);
+
+            bool[,] field = Game.GrowParallel();
+
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    if (field[x, y])
+                    {
+                        graphics.FillRectangle(Brushes.Crimson, x * resolution, y * resolution, resolution, resolution);
+                    }
+                }
+            }
+
+            pictureBox1.Refresh();
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+                return;
+
+            numResolution.Enabled = false;
+            numDensity.Enabled = false;
+            resolution = (int)numResolution.Value;
+            rows = pictureBox1.Height / resolution;
+            cols = pictureBox1.Width / resolution;
+            Game = new GameLife(cols, rows);
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+            timer1.Start();
+        }
+
+        private void bStop_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+                return;
+            timer1.Stop();
+            numResolution.Enabled = true;
+            numDensity.Enabled = true;
+        }
+    }
+}
---
+++ LaboratoryWorkFive/GameLife.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryWorkFive
+{
+    // Реализуйте последовательный алгоритм расчета состояний модели.
+    public class GameLife {
+        private int Heigth;
+        private int Width;
+        private bool[,] cells;
+
+        public GameLife(int Heigth, int Width) {
+            this.Heigth = Heigth;
+            this.Width = Width;
+            cells = new bool[Heigth, Width];
+            GenerateField();
+        }
+
+        public bool[,] Grow() {
+            for (int i = 0; i < Heigth; i++) {
+                for (int j = 0; j < Width; j++) {
+                    int numOfAliveNeighbors = GetNeighbors(i, j);
+
+                    if (cells[i, j]) {
+                        if (numOfAliveNeighbors < 2) {
+                            cells[i, j] = false;
+                        }
+
+                        if (numOfAliveNeighbors > 3) {
+                            cells[i, j] = false;
+                        }
+                    }
+                    else {
+                        if (numOfAliveNeighbors == 3) {
+                            cells[i, j] = true;
+                        }
+                    }
+                }
+            }
+
+            return cells;
+        }
+
+        public bool[,] GrowParallel()
+        {
+            Parallel.For(0, Heigth, (i) =>
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    int numOfAliveNeighbors = GetNeighbors(i, j);
+
+                    if (cells[i, j])
+                    {
+                        if (numOfAliveNeighbors < 2)
+                        {
+                            cells[i, j] = false;
+                        }
+
+                        if (numOfAliveNeighbors > 3)
+                        {
+                            cells[i, j] = false;
+                        }
+                    }
+                    else
+                    {
+                        if (numOfAliveNeighbors == 3)
+                        {
+                            cells[i, j] = true;
+                        }
+                    }
+                }
+            }
+            );
+
+            return cells;
+        }
+
+        private int GetNeighbors(int x, int y) {
+            int NumOfAliveNeighbors = 0;
+
+            for (int i = x - 1; i < x + 2; i++) {
+                for (int j = y - 1; j < y + 2; j++) {
+                    if (!((i < 0 || j < 0) || (i >= Heigth || j >= Width))) {
+                        if (cells[i, j] == true) NumOfAliveNeighbors++;
+                    }
+                }
+            }
+            return NumOfAliveNeighbors;
+        }
+
+        private void GenerateField() {
+            Random generator = new Random();
+            int number;
+            for (int i = 0; i < Heigth; i++) {
+                for (int j = 0; j < Width; j++) {
+                    number = generator.Next(2);
+                    cells[i, j] = ((number == 0) ? false : true);
+                }
+            }
+        }
+    }
+}
